e6427ba11dadde0acc89c042fa1b8d205be2f549
KJH <daeding7@naver.com>
2022-01-03 20:27:08 +0900

    로그인 실패시, 그 실패 사유를 분석해서 토스트 띄우기

--- app/src/main/java/com/kim/colosseum_kim2/MainActivity.kt
+++ app/src/main/java/com/kim/colosseum_kim2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kim.colosseum_kim2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.kim.colosseum_kim2.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
@@ -28,9 +29,29 @@ class MainActivity : BaseActivity() {
 ServerUtil.postRequestLogin(inputEmail, inputPw, object : ServerUtil.Companion.JsonReponseHandler{
     override fun onResponse(jsonObj: JSONObject) {
 
+
+
 //        jsonObj : 서버에서 내려준 본문을 JSON형태로 가공해준 결과물.
 //        이 내부를 파싱(분석)해서, 상황에 따른 대응.
 //        ex. 로그인 실패시, 그 이유를 토스트로 띄운다던지.
+
+        val code = jsonObj.getInt("code")
+
+        if(code == 200){
+
+            //로그인 성공
+        }
+
+        else{
+
+//            로그인 실패
+
+            val message = jsonObj.getString("message")
+
+           runOnUiThread {   Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show() }
+
+
+        }
     }
 
 
