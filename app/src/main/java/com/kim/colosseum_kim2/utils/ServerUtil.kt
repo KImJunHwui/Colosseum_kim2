e7a065e0666a25f236de3e5435930cb070a01883
KJH <daeding7@naver.com>
2022-01-03 19:42:31 +0900

    로그인 화면 기본 구조

--- app/src/main/java/com/kim/colosseum_kim2/utils/ServerUtil.kt
+++ app/src/main/java/com/kim/colosseum_kim2/utils/ServerUtil.kt
@@ -2,5 +2,20 @@ package com.kim.colosseum_kim2.utils
 
 class ServerUtil {
 
-//    어떤 내용 ? => 서버 연결 전담담
+//    어떤 내용 ? => 서버 연결 전담.
+
+    companion object{
+
+//        모든 기능의 기본이 되는 주소
+        val BASE_URL = "http://54.180.52.26"
+
+//        로그인 하는 기능
+
+        fun postRequestLogin(){
+
+
+        }
+
+
+    }
 }
