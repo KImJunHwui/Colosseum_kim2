9cd0eadecc3bd611f89a5db99813925ce6a9c373
KJH <daeding7@naver.com>
2022-01-10 20:45:02 +0900

    서버에 가기 전에, 주소 / 파람비터를 결합한 형태의 재료 생성

--- app/src/main/java/com/kim/colosseum_kim2/MainActivity.kt
+++ app/src/main/java/com/kim/colosseum_kim2/MainActivity.kt
@@ -2,6 +2,10 @@ package com.kim.colosseum_kim2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kim.colosseum_kim2.utils.ServerUtil
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlinx.android.synthetic.main.activity_sign_up.emailEdt
 
 class MainActivity : BaseActivity() {
 
@@ -14,6 +18,8 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+
 
     }
 
