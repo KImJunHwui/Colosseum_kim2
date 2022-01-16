662515330b886f58eb04ac79e35fa08d1bc93834
KJH <daeding7@naver.com>
2022-01-16 20:22:12 +0900

    JSONObject 재료로 => Topic으로 변환해주는 함수 추가

--- app/src/main/java/com/kim/colosseum_kim2/MainActivity.kt
+++ app/src/main/java/com/kim/colosseum_kim2/MainActivity.kt
@@ -7,6 +7,7 @@ import com.kim.colosseum_kim2.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import kotlinx.android.synthetic.main.activity_sign_up.emailEdt
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -35,6 +36,36 @@ class MainActivity : BaseActivity() {
     fun getTopicListFromServer(){
 
 //        서버에서 주제 목록을 받아오자. => /v2/main_info = ServerUtil에 기능 추가 필요.
+
+        ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.Companion.JsonReponseHandler{
+            override fun onResponse(jsonObj: JSONObject) {
+
+//                서버에서 주메목록을 받아온 상황.
+
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicsArr = dataObj.getJSONArray("topics")
+
+//               topicArr 안에 있는 여러개의 주제들을 반복적으로 파싱. => for문을 활용하자.
+
+//               배열에 15개의 주제 : index -> 0 ~ 14 까지
+                for( index in 0 until topicsArr.length()){
+
+//                    index위치에 맞는 주제들을 Topic클래스 현태로 변환.
+
+
+
+//                    mTopicList 추가.
+
+                }
+
+
+
+
+
+            }
+
+
+        })
     }
 
 }
--- app/src/main/java/com/kim/colosseum_kim2/datas/Topic.kt
+++ app/src/main/java/com/kim/colosseum_kim2/datas/Topic.kt
@@ -1,8 +1,28 @@
 package com.kim.colosseum_kim2.datas
 
+import org.json.JSONObject
+
 class Topic {
 
     var id = 0 //나중에 Int가 들어올 것이라는 명시.
     var title = "" // 나중에 String이 들어올 것이라는 명시
     var imageURL = ""
+
+    companion object{
+
+//        적당한 JSONObject 하나를 넣어주면 => Topic 형태로 변환해 주는 함수를 작성.
+
+        fun getTopicDataFromJson(jsonObj : JSONObject) : Topic{
+
+            val resultTopic = Topic()
+
+            resultTopic.id = jsonObj.getInt("id")
+            resultTopic.title = jsonObj.getString("title")
+            resultTopic.imageURL = jsonObj.getString("img_url")
+
+            return resultTopic
+        }
+
+    }
+
 }
