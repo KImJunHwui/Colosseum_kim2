9f99fb89d04c9a6366cda62914a8995827949de5
KJH <daeding7@naver.com>
2022-01-18 21:43:20 +0900

    주제 상세 화면 초안 작성 중 - 토론 진행 상황 ( 선택 진영 / 특표수 ) 미작업

--- app/src/main/java/com/kim/colosseum_kim2/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/kim/colosseum_kim2/ViewTopicDetailActivity.kt
@@ -2,8 +2,14 @@ package com.kim.colosseum_kim2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.kim.colosseum_kim2.datas.Topic
+import kotlinx.android.synthetic.main.activity_view_topic_detail.*
 
 class ViewTopicDetailActivity : BaseActivity() {
+
+    lateinit var mTopic : Topic
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_topic_detail)
@@ -16,6 +22,10 @@ class ViewTopicDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mTopic = intent.getSerializableExtra("topic") as Topic
+        topicTitleTxt.text = mTopic.title
+        Glide.with(mContext).load(mTopic.imageURL).into(topicImg)
 
     }
 
