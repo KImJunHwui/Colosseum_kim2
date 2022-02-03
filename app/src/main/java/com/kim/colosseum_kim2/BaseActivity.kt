6ce3f611eb8e3621b81d0d2dacbe95ddfcc867e2
KJH <daeding7@naver.com>
2022-02-03 21:26:07 +0900

    상속을 이용해서 => 모든 화면의 액션바를 일괄 변경

--- app/src/main/java/com/kim/colosseum_kim2/BaseActivity.kt
+++ app/src/main/java/com/kim/colosseum_kim2/BaseActivity.kt
@@ -1,11 +1,14 @@
 package com.kim.colosseum_kim2
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity(){
+
+    lateinit var backBtn : ImageView
 
     val mContext = this
 
@@ -24,10 +27,13 @@ abstract class BaseActivity : AppCompatActivity(){
         defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         defaultActionBar.setCustomView(R.layout.my_custom_action_bar)
 
-        val myToolbar = defaultActionBar.customView.parent as Toolbar
+        val myToolbar = defaultActionBar.customView.parent as androidx.appcompat.widget.Toolbar
         myToolbar.setContentInsetsAbsolute(0,0)
+
+
 
 
     }
 
-}
+    }
+
